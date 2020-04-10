cb8f3b86818999214b8c9b83a477d5812e8b7a7c
23156436 <jordanayan@live.com>
2020-04-10 18:10:04 -0500

    Added Run and Update scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+nohup python3 /home/Minecraft-ACI-Interface/main.py
---
+++ update.sh
@@ -0,0 +1,5 @@
+sudo git stash
+sudo git pull https://github.com/ACRONYM-Group/Minecraft-ACI-Interface.git
+
+sudo chmod +x update.sh
+sudo chmod +x run.sh
