fc0c666252b6ef9c0620f5475e209269c1a92102
23156436 <jordanayan@live.com>
2020-04-11 23:42:16 -0500

    Added Commands and Log access

--- main.py
+++ main.py
@@ -14,6 +14,29 @@ print(" ")
 
 resp = mcr.command("/testforblock 1253 12 626 651")
 
+ def tail(f, lines=20):
+    total_lines_wanted = lines
+
+    BLOCK_SIZE = 1024
+    f.seek(0, 2)
+    block_end_byte = f.tell()
+    lines_to_go = total_lines_wanted
+    block_number = -1
+    blocks = []
+    while lines_to_go > 0 and block_end_byte > 0:
+        if (block_end_byte - BLOCK_SIZE > 0):
+            f.seek(block_number*BLOCK_SIZE, 2)
+            blocks.append(f.read(BLOCK_SIZE))
+        else:
+            f.seek(0,0)
+            blocks.append(f.read(block_end_byte))
+        lines_found = blocks[-1].count(b'\n')
+        lines_to_go -= lines_found
+        block_end_byte -= BLOCK_SIZE
+        block_number -= 1
+    all_read_text = b''.join(reversed(blocks))
+    return b'\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+
 while True:
     resp = mcr.command("/testforblock 1253 12 626 651")
     if resp[0] == "S":
@@ -35,6 +58,15 @@ while True:
     if conn["minecraft"]["stop"] == "True":
         conn["minecraft"]["stop"] = "False"
         mcr.command("/stop")
+
+    commandToExecute = conn["minecraft"]["command"]
+    if commandToExecute != "":
+        mcr.command(commandToExecute)
+        conn["minecraft"]["command"] = ""
+
+    file = open("/home/blightfall/server/logs/latest.log")
+    conn["minecraft"]["log"] = tail(file)
+    file.close()
 
     
     time.sleep(1)
